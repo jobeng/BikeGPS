63a85dbac77a4b36055df6c40f97f1e65f644995
jobeng <jessie.o.obeng@gmail.com>
2016-10-13 17:48:15 -0400

    2nd Copy

--- app/src/main/java/cisc340/wheretojack/ledCompass.java
+++ app/src/main/java/cisc340/wheretojack/ledCompass.java
@@ -1,18 +1,29 @@
 package cisc340.wheretojack;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.util.UUID;
+
 
 
 public class ledCompass extends Activity {
 
     Button searchButton;
     EditText addressField;
+
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket isBtConnected = null;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     String SEARCH_ADDRESS;
 
